90251ab0ff6852f211faa04b6950dfd1a1df865e
Nimra Siddique <151427591+nimrakhan190@users.noreply.github.com>
2026-02-19 09:38:13 +0500

    Day 1 - Band Name Generator project
    Documenting my 100 Days of Python journey. Daily projects, exercises, and concepts as I build consistency and strengthen my problem-solving skills.

---
+++ Day-01/bandname_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name!")
+city = input("What's your city name where you grew up?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name is: " + city + " " + pet)
